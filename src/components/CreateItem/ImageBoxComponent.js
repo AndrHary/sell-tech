5b0944086127f80b9cf3b76a9a0b849b8f4147f2
Andriyan Harizanov <81181412+AndrHary@users.noreply.github.com>
2021-12-14 15:43:41 +0200

    Removed not needed images

--- src/components/CreateItem/ImageBoxComponent.js
+++
@@ -1,9 +0,0 @@
-function ImageInputComponent({ image }) {
-  return (
-
-    <div className="input-image-cont">
-       <img src={image} alt="item"></img>
-    </div>
-  )
-}
-export default ImageInputComponent
